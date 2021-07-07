d0b5d22a33bf9d8ffcb2983db17f8e4948125c0f
Hua Gong <huagong@ITAdmins-MacBook-Pro.local>
2021-07-07 15:46:31 -0500

    update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Amplify, { API } from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+// Amplify init
+Amplify.configure(awsconfig);
+API.configure(awsconfig);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,6 +15,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
